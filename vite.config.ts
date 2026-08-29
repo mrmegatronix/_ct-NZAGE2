4a4f8d1bd1dfb3f496cb08f35dba4041db0ff1c8
mrmegatronix <whitecomet2014@gmail.com>
2026-08-30 06:32:26 +1200

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/_ct-NZAGE2/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
